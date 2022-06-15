3c99ce6e7f0ad8de5f2cae2887372d9978c0f67e
Shunsuke Fujishima <z3033139@edu.gifu-u.ac.jp>
2022-06-15 12:02:16 +0900

    今日はAndroid Studioのエラーと戦っていたのでプログラムの枠組みだけ作って終わりました。来週までにある程度の関数や、処理を考えます。

---
+++ app/src/main/java/jp/ac/gifu_u/z3033139/last/Lasted.java
@@ -0,0 +1,10 @@
+package jp.ac.gifu_u.z3033139.last;
+
+import android.content.Context;
+import android.view.View;
+
+public class Lasted extends View {
+    public Lasted(Context context) {
+        super(context);
+    }
+}
---
+++ app/src/main/java/jp/ac/gifu_u/z3033139/last/MainActivity.java
@@ -0,0 +1,22 @@
+package jp.ac.gifu_u.z3033139.last;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    //private MyView myView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    //ボタンが推された時の動作用
+
+}
